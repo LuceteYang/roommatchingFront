a1a0d6a2899f48ca1284e8094aa72b80d311e11d
rooney11 <jae6120@naver.com>
2016-08-10 23:51:45 +0900

    routing 추가

--- src/Components/PageTwo.js
+++ src/Components/PageTwo.js
@@ -2,10 +2,28 @@
  * Created by User on 2016-08-07.
  */
 import React from 'react';
+import axios from 'axios';
 class PageTwo extends React.Component {
+    onClick() {
+        axios.get('https://roommatching-rooney11.c9users.io/auth/facebook')
+            .then( response => {
+                var myWindow = window.open("", "MsgWindow", "width=200,height=100");
+                myWindow.document.write(response.data); } ) // SUCCESS
+            .catch( response => { console.log(response); } ); // ERROR
+    }
+    logoutClick(){
+        axios.get('https://roommatching-rooney11.c9users.io/sign_out')
+            .then( response => {
+               alert(response.data); } ) // SUCCESS
+            .catch( response => { console.log(response); } ); // ERROR
+    }
     render() {
         return (
-            <h2>Hey, I am PageTwo!</h2>
+            <div>
+                <button type="button" onClick={this.onClick}>Click Me</button>
+                <button type="button" onClick={this.logoutClick}>Log out</button>
+                <h2>Hey, I am PageTwo!</h2>
+            </div>
         );
     }
 }
