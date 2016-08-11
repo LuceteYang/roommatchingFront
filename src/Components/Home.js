e1039cadc5cf565610fbad4bdb0310c785a3b6cb
rooney11 <jae6120@naver.com>
2016-08-11 17:05:00 +0900

    routing 추가

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,8 +4,29 @@
 import React from 'react';
 class Home extends React.Component {
     render() {
+        var sectionStyle = {
+            color:'#ff4444'
+        }
         return (
-            <h2>Hey, I am Home!</h2>
+            <div>
+                <h2><span style={sectionStyle}> ></span> 학교</h2>
+                <h2>충남대학교"</h2>
+                <h2><span style={sectionStyle}>></span> 기간</h2>
+                <div class="checkbox">
+                    <label><input type="checkbox" onClick="return KeepCount()" id="four" name="fourthdong" value="1" />하계 1차</label>
+                    <label><input type="checkbox" onClick="return KeepCount()" id="four" name="fourthdong" value="2" />하계 2차</label>
+                </div>
+                <h2><span style={sectionStyle}>></span> 희망동 선택</h2>
+                <div class="checkbox">
+                    <label><input type="checkbox" name="fourthdong" value="1" />1 동</label>
+                    <label><input type="checkbox"  name="fourthdong" value="2" />2 동</label>
+                    <label><input type="checkbox" name="fourthdong" value="3" />3 동</label>
+                    <label><input type="checkbox"  name="fourthdong" value="4" />4 동</label>
+                    <label><input type="checkbox"  name="fourthdong" value="8" />8 동</label>
+                    <label><input type="checkbox"  name="fourthdong" value="9" />9 동</label>
+                </div>
+                <button>저장</button>
+            </div>
         );
     }
 }
--- src/Components/PageTwo.js
+++ src/Components/PageTwo.js
@@ -3,23 +3,38 @@
  */
 import React from 'react';
 import axios from 'axios';
+import {RadioGroup, Radio} from 'react-radio-group';
 class PageTwo extends React.Component {
+    getInitialState() {
+        return {selectedValue: 'apple'};
+    }
+
+    handleChange(value) {
+        this.setState({selectedValue: value});
+    }
     onClick() {
         axios.get('https://roommatching-rooney11.c9users.io/auth/facebook')
             .then( response => {
-                var myWindow = window.open("", "MsgWindow", "width=200,height=100");
+                var myWindow = window.open("", "MsgWindow", "width=500,height=500");
                 myWindow.document.write(response.data); } ) // SUCCESS
             .catch( response => { console.log(response); } ); // ERROR
     }
     logoutClick(){
         axios.get('https://roommatching-rooney11.c9users.io/sign_out')
             .then( response => {
-               alert(response.data); } ) // SUCCESS
+                alert(response.data); } ) // SUCCESS
             .catch( response => { console.log(response); } ); // ERROR
     }
     render() {
         return (
             <div>
+                <h2>언제 일어나나요?</h2>
+                <form name="fruit" selectedValue={this.state.selectedValue} onChange={this.handleChange}>
+                    <input type="radio" name="fruit" value="apple" />올빼미형 9시 이후
+                    <input type="radio" name="fruit" value="orange" />아침형 8시 이전
+                    <input type="radio" name="fruit" value="watermelon" />보통형 8시 이후 9시 이전
+                    <input type="radio" name="fruit" value="watermelon" />상관없어요
+                </form>
                 <button type="button" onClick={this.onClick}>Click Me</button>
                 <button type="button" onClick={this.logoutClick}>Log out</button>
                 <h2>Hey, I am PageTwo!</h2>
