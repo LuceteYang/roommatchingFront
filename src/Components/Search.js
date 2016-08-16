ea1a25d5f9cfc69d4cf05347c3564d1834b941c4
kiss9815 <kiss9815@naver.com>
2016-08-16 22:31:00 +0900

    juntae ajax test

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -2,12 +2,51 @@
  * Created by User on 2016-08-07.
  */
 import React from 'react';
-class Search extends React.Component {
-    render() {
+
+
+var Search = React.createClass({
+      getInitialState: function() {
+            return {
+            username: 'jjj',
+            lastGistUrl: '',
+            source : 'https://api.github.com/users/octocat/gists',
+            searchResults : []
+            };
+        },
+    
+    componentDidMount: function() {
+        this.serverRequest = $.get(this.state.source, function (result) {
+        var lastGist = result[0];
+        this.setState({
+            username: lastGist.owner.login,
+            lastGistUrl: lastGist.html_url
+        });
+        }.bind(this));
+ 	},
+    hi : function(){
+        alert('dddd');
+    },
+
+  componentWillUnmount: function() {
+    this.serverRequest.abort();
+  },
+
+    render : function(){
+        console.log('test', this.state.searchResults);
         return (
-            <h2>Hey, I am Search!</h2>
+              <div>
+                    <h2>Hey, I am Search!</h2>
+
+
+                    {this.state.username}'s last gist is
+                    
+                    
+                    <h3 >{this.state.lastGistUrl}</h3>
+                    
+
+            </div>
         );
     }
-}
+});
 
 export default Search;
