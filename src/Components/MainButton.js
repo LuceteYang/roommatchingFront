1836285e1ad8c6bc5f92867f36001c03f98aa27e
kiss9815 <kiss9815@naver.com>
2016-08-23 19:36:53 +0900

    juntae server test login

--- src/Components/MainButton.js
+++ src/Components/MainButton.js
@@ -2,118 +2,6 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 
-// var MainButton = React.createClass({
-//      getInitialState: function() {
-//             return {
-//             username: '',
-//             lastGistUrl: '',
-//             source : 'http://52.78.90.9/home',
-//             searchResults : [],
-//             data : ''
-//             };
-//         },
-    // https://api.github.com/users/octocat/gists
-    // https://roommatching-rooney11.c9users.io/home
-    // componentDidMount: function() {
-    //     this.serverRequest = $.get(this.state.source, function (result) {
-    //         console.log('result', result);
-    //     var lastGist = result[0];
-    //     this.setState({
-    //         username: lastGist.owner.login,
-    //         lastGistUrl: lastGist.html_url
-    //     });
-    //     }.bind(this));
- 	// },
-    // componentDidMount: function() {
-    //    this.serverRequest = $.ajax({
-    //         url: this.state.source,
-    //         type: 'GET',
-    //         dataType: 'json',
-    //         crossDomain : true,
-    //         cache: false,
-    //         success: function(data) {
-    //             console.log('data', data);
-    //             this.setState({data: data});
-    //         }.bind(this),
-    //         error: function(xhr, status, err) {
-    //             console.error(this.state.source, status, err.toString());
-    //         }.bind(this)
-    //     });
-    // },
-     
-    // // componentDidMount: function() {
-    // //    axios.get('https://roommatching-rooney11.c9users.io/home').then(response => {
-    // //           console.log('response', response);
-    // //            this.props.onReceive(response.data.number);
-    // //            setTimeout(getNumber, 1000 * 5); // REPEAT THIS EVERy 5 SECONDS
-    // //         });
-    // // },
-
-    // componentWillUnmount: function() {
-    //     this.serverRequest.abort();
-    // },
-    
-    
-    
-    // handleClick: function() {
-    //     this.setState({
-    //         isSelected: true,
-            
-    //     }),
-    //     alert("Hello World!");
-    // },
-    // openModal: function(){
-    //         // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    //         $('.modal-trigger').leanModal();
-    // },
-    // loginFaceBook : function(){
-    //      this.serverRequest = $.ajax({
-    //         url: 'http://52.78.90.9/users/auth/facebook',
-    //         type: 'GET',
-    //         dataType: 'json',
-    //         crossDomain : true,
-    //         cache: false,
-    //         success: function(data) {
-    //             console.log('facebook', data);
-    //             this.setState({facebook: data});
-    //         }.bind(this),
-    //         error: function(xhr, status, err) {
-    //             console.error(this.state.source, status, err.toString());
-    //         }.bind(this)
-    //     });
-    // },
-
-
-//     render : function(){
-//         console.log('state', this.state.data);
-//         console.log('state data', this.state.data.is_login);
-
-//         var loginButton;
-//         if(this.state.data.is_login){
-//             loginButton = "로그인 했음";
-//         }else{
-//             loginButton = "로그인 안함";
-//         }
-
-//         return (
-            
-//             <div style = {mainButtonStyle}>
-//                 <a className="modal-trigger waves-effect waves-light btn" href="#modal1" onClick = {this.loginFaceBook}>{loginButton}</a>
-                
-//                     <div id="modal1" className="modal">
-//                         <div className="modal-content">
-//                         <h4>Modal Header</h4>
-//                         <p>A bunch of text</p>
-//                         </div>
-//                         <div className="modal-footer">
-//                         <a href="#!" className=" modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
-//                         </div>
-//                     </div>
-//             </div>
-           
-//         );
-//     }
-// });
 var mainButtonStyle = {
        textAlign : 'center',
        margin : 'auto',
@@ -153,6 +41,7 @@ class MainButton extends React.Component {
          };
          this.loginFaceBook = this.loginFaceBook.bind(this);
         // this._updateValue = this._updateValue.bind(this);
+        this.onClick = this.onClick.bind(this);
     }
 
     componentDidMount(){
@@ -177,34 +66,54 @@ class MainButton extends React.Component {
                 this.props.onReceive(response.data);
             });
         }
-
+        
         isLoginedButton();
 
-        let isFaceBookLogin = function(){
-            axios.get('http://roommatching.xyz/auth/facebook').then(response => {
-                console.log('componentDidMount', response.data.is_login);
-                this.props.onReceive(response.data);
-            });
-        }
-        isFaceBookLogin();
+        
+        
+
     }
+    
+    isFaceBookLogin(){
+            axios({url :'http://roommatching.xyz/home',
+                    method :'get',
+                    headers : {'Access-Control-Allow-Origin': "*", 
+                                "Access-Control-Allow-Headers": "x-requested-with",
+                                "Access-Control-Max-Age": "3600"
+                            }
+                    }
+                ).then(response => {
+                try{
+                    JSON.parse(response.data);
+                }catch(e){
+                    var myWindow = window.open("", "MsgWindow", "width=500,height=500");
+                    myWindow.document.write(response.data);
+                    return false;
+                }
+                     console.log('componentDidMount', response.data);
+                    this.props.onReceive(response.data);
+// 아까 그게 뭐예요?? 페북 화면 작은 화면 로그인 창 이뜨는
+                     return true;
+                })
+               
+            }
     componentWillUnmount(){
          this.serverRequest.abort();
     }
 
-openModal(){
-            // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-            $('.modal-trigger').leanModal();
-    }
+    openModal(){
+                // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+                $('.modal-trigger').leanModal();
+        }
     loginFaceBook(){
           $.ajax({
-            url: 'http://roommatching.xyz/auth/facebook',
+            url: 'http://roommatching.xyz/home',
             type: 'GET',
             dataType: 'json',
             crossDomain : true,
             cache: false,
             success: function(data) {
-                console.log('facebook', data);
+                console.log('facebook success ajax', data);
                 this.setState({facebook: data});
             }.bind(this),
             error: function(xhr, status, err) {
@@ -212,8 +121,11 @@ openModal(){
             }.bind(this)
         });
     }
-
-
+    onClick(){
+        // this.isFaceBookLogin();
+        this.loginFaceBook();
+        console.log("dddd")
+    }
     render(){
         
         console.log('state', this.state.data);
@@ -230,7 +142,7 @@ openModal(){
         return (
             
             <div style = {mainButtonStyle}>
-                <a className="modal-trigger waves-effect waves-light btn" href="#modal1" onClick = {this.openModal}>{loginButton}</a>
+                <a className="modal-trigger waves-effect waves-light btn" href="#modal1" onClick = {this.onClick}>{loginButton}</a>
                 
                     <div id="modal1" className="modal">
                         <div className="modal-content">
