21101ce5c7c762da90b2029976f6b2497728779f
kiss9815 <kiss9815@naver.com>
2016-08-24 23:10:39 +0900

    juntae facebook ajax 2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { createStore } from 'redux';
+import { Provider  } from 'react-redux';
+import counterApp from './reducers'; // reducers 폴더에서 actions 폴더를 사용
+const store = createStore(counterApp);
+
 import App from './App';
 import App2 from './App2';
 import Main from './Main';
@@ -11,15 +17,18 @@ import { Router, Route, browserHistory, IndexRoute} from 'react-router';
 import './index.css';
 
 ReactDOM.render(
-    <Router history = {browserHistory}>
-        <Route path = "/" component = {App}>
-         <IndexRoute component = {App2} />
-        <Route path = "/main" component = {Main}>
-           
-           
-        </Route>
-        </Route>
-    </Router>,
+    <Provider store={store}>
+
+        <Router history = {browserHistory}>
+            <Route path = "/" component = {App}>
+            <IndexRoute component = {App2} />
+            <Route path = "/main" component = {Main}>
+            
+            
+            </Route>
+            </Route>
+        </Router>
+    </Provider>,
   document.getElementById('root')
 );
 
