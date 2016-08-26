3e347ad3545fe8db43f56242d6aa5c9098e36135
kiss9815 <kiss9815@naver.com>
2016-08-26 20:53:21 +0900

    juntae UI footer change content little

--- src/Components/HomeFooter.js
+++ src/Components/HomeFooter.js
@@ -40,17 +40,17 @@ class HomeFooter extends React.Component {
                         <div className="col l3 s12">
                         <h5 className="white-text">Connect</h5>
                         <ul>
-                            <li><a className="white-text" href="#!">Link 1</a></li>
-                            <li><a className="white-text" href="#!">Link 2</a></li>
-                            <li><a className="white-text" href="#!">Link 3</a></li>
-                            <li><a className="white-text" href="#!">Link 4</a></li>
+                            <li><a className="white-text" href="#!">개발자 덕명동 남자</a></li>
+                            <li><a className="white-text" href="#!">CTO 박상환</a></li>
+                            <li><a className="white-text" href="#!">알고리즘 전문가 김태희</a></li>
+                            <li><a className="white-text" href="#!">총괄 기획 팀장 진하윤</a></li>
                         </ul>
                         </div>
                     </div>
                     </div>
                     <div className="footer-copyright">
                     <div className="container">
-                    Made by <a className="brown-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+                    Made by <a className="brown-text text-lighten-3" target="_blank" href="https://www.facebook.com/likelioncnu/?fref=nf">멋쟁이 사자처럼 충남대</a>
                     </div>
                     </div>
                 </footer>
