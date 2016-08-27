4333f80f4a5819992c2e54730e6fb7c435edcfdf
rooney11 <jae6120@naver.com>
2016-08-27 17:57:31 +0900

    Tab 부분 수정

--- src/Components/Tab.js
+++ src/Components/Tab.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 
-import Home from './Home';
+import First from './First';
 import PageTwo from './PageTwo';
 import ResultTab from './ResultTab';
 
@@ -13,15 +13,15 @@ class Tab extends React.Component{
                          <div className="col s12">
                          <ul className="tabs">
                              <li className="tab col s3"><a  href="#test1">Test 1</a></li>
-                            <li className="tab col s3"><a href="#test2 ">설문 조사 하기</a></li>
-                            <li className="tab col s3"><a className="active" href="#test4">매칭 결과 보기</a></li>
+                            <li className="tab col s3"><a className="active" href="#test2 ">설문 조사 하기</a></li>
+                            <li className="tab col s3"><a  href="#test4">매칭 결과 보기</a></li>
                         </ul>
                         </div>
-                        <div id="test1" className="col s12"><Home/></div>
+                        <div id="test1" className="col s12"><First/></div>
                         <div id="test2" className="col s12"><PageTwo/></div>
                         <div id="test4" className="col s12"><ResultTab url="https://api.github.com/users/octocat/gists"/></div>
-             
-                         
+
+
              </div>
         );
     }
